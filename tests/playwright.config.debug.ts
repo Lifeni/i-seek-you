6d558cd89f4337f6866bcb6ac86469fa02e0d5c5
梁峰宁 <liangfengning@foxmail.com>
2022-08-20 16:01:23 +0800

    test: remove unfinished tests

--- tests/playwright.config.debug.ts
+++
@@ -1,6 +0,0 @@
-import { PlaywrightTestConfig } from '@playwright/test'
-const config: PlaywrightTestConfig = {
-  use: { channel: 'chrome', baseURL: 'http://localhost:8080', headless: false },
-  workers: 1,
-}
-export default config
--- tests/playwright.config.ts
+++
@@ -1,6 +0,0 @@
-import { PlaywrightTestConfig } from '@playwright/test'
-const config: PlaywrightTestConfig = {
-  use: { channel: 'chrome', baseURL: 'https://i-seek-you.dist.run' },
-  workers: 3,
-}
-export default config
